90757722d0a1571886a70274326b39abf1131f07
demersson barros <demerson.b@hotmail.com>
2023-10-28 20:12:42 -0300

    Feat: Adicionado logo no header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,12 @@
 import * as S from './styles'
+import rocket from '../../assets/rocket.svg'
+import todo from '../../assets/todo.svg'
 
 export const Header = () => {
-  return <S.ContainerHeader />
+  return (
+    <S.ContainerHeader>
+      <img src={rocket} alt="" />
+      <img src={todo} alt="" />
+    </S.ContainerHeader>
+  )
 }
--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -4,5 +4,10 @@ export const ContainerHeader = styled.header`
   width: 100%;
   height: 12.5rem;
 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+
   background: ${(props) => props.theme['gray-700']};
 `
